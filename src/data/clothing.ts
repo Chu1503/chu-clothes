dede97dbe557cc6090226413be86084cfc3c0f8a
Chu1503 <shivakash2003@gmail.com>
2025-08-20 22:07:29 +0530

    tees, tracks, and more

--- src/data/clothing.ts
+++ src/data/clothing.ts
@@ -1,66 +1,131 @@
-import pants1 from "@/assets/pants-1.png";
-import pants2 from "@/assets/pants-2.png";
-import pants3 from "@/assets/pants-3.png";
-import pants4 from "@/assets/pants-4.png";
-import pants5 from "@/assets/pants-5.png";
-import pants6 from "@/assets/pants-6.png";
-import pants7 from "@/assets/pants-7.png";
-import pants8 from "@/assets/pants-8.png";
-import pants9 from "@/assets/pants-9.png";
+import pants1 from "@/assets/Pants/pants-1.png";
+import pants2 from "@/assets/Pants/pants-2.png";
+import pants3 from "@/assets/Pants/pants-3.png";
+import pants4 from "@/assets/Pants/pants-4.png";
+import pants5 from "@/assets/Pants/pants-5.png";
+import pants6 from "@/assets/Pants/pants-6.png";
+import pants7 from "@/assets/Pants/pants-7.png";
+import pants8 from "@/assets/Pants/pants-8.png";
+import pants9 from "@/assets/Pants/pants-9.png";
+import pants10 from "@/assets/Pants/pants-10.png";
+import pants11 from "@/assets/Pants/pants-11.png";
+import pants12 from "@/assets/Pants/pants-12.png";
+import pants13 from "@/assets/Pants/pants-13.png";
 
 
-import polo1 from "@/assets/polo-1.png";
-import polo2 from "@/assets/polo-2.png";
-import polo3 from "@/assets/polo-3.png";
-import polo4 from "@/assets/polo-4.png";
-import polo5 from "@/assets/polo-5.png";
-import polo6 from "@/assets/polo-6.png";
-import polo7 from "@/assets/polo-7.png";
-import polo8 from "@/assets/polo-8.png";
-import polo9 from "@/assets/polo-9.png";
-import polo10 from "@/assets/polo-10.png";
-import polo11 from "@/assets/polo-11.png";
-import polo12 from "@/assets/polo-12.png";
-import polo13 from "@/assets/polo-13.png";
-import polo14 from "@/assets/polo-14.png";
-import polo15 from "@/assets/polo-15.png";
-import polo16 from "@/assets/polo-16.png";
+import polo1 from "@/assets/Polos/polo-1.png";
+import polo2 from "@/assets/Polos/polo-2.png";
+import polo3 from "@/assets/Polos/polo-3.png";
+import polo4 from "@/assets/Polos/polo-4.png";
+import polo5 from "@/assets/Polos/polo-5.png";
+import polo6 from "@/assets/Polos/polo-6.png";
+import polo7 from "@/assets/Polos/polo-7.png";
+import polo8 from "@/assets/Polos/polo-8.png";
+import polo9 from "@/assets/Polos/polo-9.png";
+import polo10 from "@/assets/Polos/polo-10.png";
+import polo11 from "@/assets/Polos/polo-11.png";
+import polo12 from "@/assets/Polos/polo-12.png";
+import polo13 from "@/assets/Polos/polo-13.png";
+import polo14 from "@/assets/Polos/polo-14.png";
+import polo15 from "@/assets/Polos/polo-15.png";
+import polo16 from "@/assets/Polos/polo-16.png";
+import polo17 from "@/assets/Polos/polo-17.png";
 
 
-import shirt1 from "@/assets/shirt-1.png";
-import shirt2 from "@/assets/shirt-2.png";
-import shirt3 from "@/assets/shirt-3.png";
-import shirt4 from "@/assets/shirt-4.png";
-import shirt5 from "@/assets/shirt-5.png";
-import shirt6 from "@/assets/shirt-6.png";
-import shirt7 from "@/assets/shirt-7.png";
-import shirt8 from "@/assets/shirt-8.png";
-import shirt9 from "@/assets/shirt-9.png";
-import shirt10 from "@/assets/shirt-10.png";
-import shirt11 from "@/assets/shirt-11.png";
-import shirt12 from "@/assets/shirt-12.png";
-import shirt13 from "@/assets/shirt-13.png";
-import shirt14 from "@/assets/shirt-14.png";
-import shirt15 from "@/assets/shirt-15.png";
-import shirt16 from "@/assets/shirt-16.png";
-import shirt17 from "@/assets/shirt-17.png";
-import shirt18 from "@/assets/shirt-18.png";
+import shirt1 from "@/assets/Shirts/shirt-1.png";
+import shirt2 from "@/assets/Shirts/shirt-2.png";
+import shirt3 from "@/assets/Shirts/shirt-3.png";
+import shirt4 from "@/assets/Shirts/shirt-4.png";
+import shirt5 from "@/assets/Shirts/shirt-5.png";
+import shirt6 from "@/assets/Shirts/shirt-6.png";
+import shirt7 from "@/assets/Shirts/shirt-7.png";
+import shirt8 from "@/assets/Shirts/shirt-8.png";
+import shirt9 from "@/assets/Shirts/shirt-9.png";
+import shirt10 from "@/assets/Shirts/shirt-10.png";
+import shirt11 from "@/assets/Shirts/shirt-11.png";
+import shirt12 from "@/assets/Shirts/shirt-12.png";
+import shirt13 from "@/assets/Shirts/shirt-13.png";
+import shirt14 from "@/assets/Shirts/shirt-14.png";
+import shirt15 from "@/assets/Shirts/shirt-15.png";
+import shirt16 from "@/assets/Shirts/shirt-16.png";
+import shirt17 from "@/assets/Shirts/shirt-17.png";
+import shirt18 from "@/assets/Shirts/shirt-18.png";
+import shirt19 from "@/assets/Shirts/shirt-19.png";
 
 
-import tshirt1 from "@/assets/tshirt-1.png";
-import tshirt2 from "@/assets/tshirt-2.png";
+import tee1 from "@/assets/Tees/tee-1.png";
+import tee2 from "@/assets/Tees/tee-2.png";
+import tee3 from "@/assets/Tees/tee-3.png";
+import tee4 from "@/assets/Tees/tee-4.png";
+import tee5 from "@/assets/Tees/tee-5.png";
+import tee6 from "@/assets/Tees/tee-6.png";
+import tee7 from "@/assets/Tees/tee-7.png";
+import tee8 from "@/assets/Tees/tee-8.png";
+import tee9 from "@/assets/Tees/tee-9.png";
+import tee10 from "@/assets/Tees/tee-10.png";
+import tee11 from "@/assets/Tees/tee-11.png";
+import tee12 from "@/assets/Tees/tee-12.png";
+import tee13 from "@/assets/Tees/tee-13.png";
+import tee14 from "@/assets/Tees/tee-14.png";
+import tee15 from "@/assets/Tees/tee-15.png";
+import tee16 from "@/assets/Tees/tee-16.png";
+import tee17 from "@/assets/Tees/tee-17.png";
+import tee18 from "@/assets/Tees/tee-18.png";
+import tee19 from "@/assets/Tees/tee-19.png";
+import tee20 from "@/assets/Tees/tee-20.png";
 
 
-import hoodie1 from "@/assets/hoodie-1.png";
-import hoodie2 from "@/assets/hoodie-2.png";
+import gtee1 from "@/assets/Graphic Tees/gtee-1.png";
+import gtee2 from "@/assets/Graphic Tees/gtee-2.png";
+import gtee3 from "@/assets/Graphic Tees/gtee-3.png";
+import gtee4 from "@/assets/Graphic Tees/gtee-4.png";
+import gtee5 from "@/assets/Graphic Tees/gtee-5.png";
+import gtee6 from "@/assets/Graphic Tees/gtee-6.png";
+import gtee7 from "@/assets/Graphic Tees/gtee-7.png";
+import gtee8 from "@/assets/Graphic Tees/gtee-8.png";
+import gtee9 from "@/assets/Graphic Tees/gtee-9.png";
+import gtee10 from "@/assets/Graphic Tees/gtee-10.png";
+import gtee11 from "@/assets/Graphic Tees/gtee-11.png";
+import gtee12 from "@/assets/Graphic Tees/gtee-12.png";
+import gtee13 from "@/assets/Graphic Tees/gtee-13.png";
+import gtee14 from "@/assets/Graphic Tees/gtee-14.png";
+import gtee15 from "@/assets/Graphic Tees/gtee-15.png";
+import gtee16 from "@/assets/Graphic Tees/gtee-16.png";
+import gtee17 from "@/assets/Graphic Tees/gtee-17.png";
+import gtee18 from "@/assets/Graphic Tees/gtee-18.png";
+import gtee19 from "@/assets/Graphic Tees/gtee-19.png";
+import gtee20 from "@/assets/Graphic Tees/gtee-20.png";
+import gtee21 from "@/assets/Graphic Tees/gtee-21.png";
+import gtee22 from "@/assets/Graphic Tees/gtee-22.png";
 
 
-import sweatshirt1 from "@/assets/sweatshirt-1.png";
-import sweatshirt2 from "@/assets/sweatshirt-2.png";
-import sweatshirt3 from "@/assets/sweatshirt-3.png";
-import sweatshirt4 from "@/assets/sweatshirt-4.png";
-import sweatshirt5 from "@/assets/sweatshirt-5.png";
-import sweatshirt6 from "@/assets/sweatshirt-6.png";
+import hoodie1 from "@/assets/Hoodies/hoodie-1.png";
+import hoodie2 from "@/assets/Hoodies/hoodie-2.png";
+import hoodie3 from "@/assets/Hoodies/hoodie-3.png";
+import hoodie4 from "@/assets/Hoodies/hoodie-4.png";
+import hoodie5 from "@/assets/Hoodies/hoodie-5.png";
+
+
+import sweatshirt1 from "@/assets/Sweatshirts/sweatshirt-1.png";
+import sweatshirt2 from "@/assets/Sweatshirts/sweatshirt-2.png";
+import sweatshirt3 from "@/assets/Sweatshirts/sweatshirt-3.png";
+import sweatshirt4 from "@/assets/Sweatshirts/sweatshirt-4.png";
+import sweatshirt5 from "@/assets/Sweatshirts/sweatshirt-5.png";
+import sweatshirt6 from "@/assets/Sweatshirts/sweatshirt-6.png";
+import sweatshirt7 from "@/assets/Sweatshirts/sweatshirt-7.png";
+import sweatshirt8 from "@/assets/Sweatshirts/sweatshirt-8.png";
+import sweatshirt9 from "@/assets/Sweatshirts/sweatshirt-9.png";
+
+
+import tracks1 from "@/assets/Tracks/tracks-1.png";
+import tracks2 from "@/assets/Tracks/tracks-2.png";
+import tracks3 from "@/assets/Tracks/tracks-3.png";
+import tracks4 from "@/assets/Tracks/tracks-4.png";
+import tracks5 from "@/assets/Tracks/tracks-5.png";
+import tracks6 from "@/assets/Tracks/tracks-6.png";
+import tracks7 from "@/assets/Tracks/tracks-7.png";
+import tracks8 from "@/assets/Tracks/tracks-8.png";
+import tracks9 from "@/assets/Tracks/tracks-9.png";
 
 export type VoteType = 'good' | 'mid' | 'bad' | null;
 
@@ -83,14 +148,18 @@ export const clothingCategories: ClothingCategory[] = [
     name: 'PANTS',
     items: [
       { id: 'pants-1', name: 'Bonkers Corner | Dark Gray Cargos', image: pants1, vote: null },
-{ id: 'pants-2', name: 'Peter England | Dark Gray Chinos', image: pants2, vote: null },
-{ id: 'pants-3', name: 'Decathlon | Dark Gray Pants', image: pants3, vote: null },
-{ id: 'pants-4', name: 'John Players | Olive Green Drawstring Chinos', image: pants4, vote: null },
-{ id: 'pants-5', name: 'Jack & Jones | Beige Chinos', image: pants5, vote: null },
-{ id: 'pants-6', name: 'H&M | Beige Drawstring Cargos', image: pants6, vote: null },
-{ id: 'pants-7', name: 'Pepe Jeans | Dark Blue Jeans', image: pants7, vote: null },
-{ id: 'pants-8', name: 'SF Jeans | Blue Jeans', image: pants8, vote: null },
-{ id: 'pants-9', name: 'Levis | Blue Jeans', image: pants9, vote: null },
+      { id: 'pants-2', name: 'Peter England | Dark Gray Chinos', image: pants2, vote: null },
+      { id: 'pants-3', name: 'Decathlon | Dark Gray Pants', image: pants3, vote: null },
+      { id: 'pants-4', name: 'John Players | Olive Green Drawstring Chinos', image: pants4, vote: null },
+      { id: 'pants-5', name: 'Jack & Jones | Beige Chinos', image: pants5, vote: null },
+      { id: 'pants-6', name: 'H&M | Beige Drawstring Cargos', image: pants6, vote: null },
+      { id: 'pants-7', name: 'Pepe Jeans | Dark Blue Jeans', image: pants7, vote: null },
+      { id: 'pants-8', name: 'SF Jeans | Blue Jeans', image: pants8, vote: null },
+      { id: 'pants-9', name: 'Levis | Blue Jeans', image: pants9, vote: null },
+      { id: 'pants-10', name: 'Easy2Wear | Black Linen Pants', image: pants10, vote: null },
+      { id: 'pants-11', name: 'Westside | White Linen Pants', image: pants11, vote: null },
+      { id: 'pants-12', name: 'Westside | Black Formal Pants', image: pants12, vote: null },
+      { id: 'pants-13', name: 'Westside | Beige Formal Pants', image: pants13, vote: null },
     ]
   },
   {
@@ -107,13 +176,13 @@ export const clothingCategories: ClothingCategory[] = [
       { id: 'polo-8', name: 'Netplay | Beige Half Sleeve Knitted', image: polo8, vote: null },
       { id: 'polo-9', name: 'Netplay | Olive Green Half Sleeve Knitted', image: polo9, vote: null },
       { id: 'polo-10', name: 'Westside | Black Half Sleeve Ribbed', image: polo10, vote: null },
-      { id: 'polo-11', name: 'India | Blue Half Sleeve Jersey', image: polo11, vote: null },
-      { id: 'polo-12', name: 'CSK | Yellow Half Sleeve Jersey', image: polo12, vote: null },
-      { id: 'polo-13', name: 'NUS | Navy Blue Half Sleeve Jersey', image: polo13, vote: null },
-      { id: 'polo-14', name: 'Superconcepts | Navy Blue Full Sleeve Ribbed', image: polo14, vote: null },
-      { id: 'polo-15', name: 'H&M | Beige Full Sleeve Ribbed', image: polo15, vote: null },
-      { id: 'polo-16', name: 'Tyre | Black & Beige Full Sleeve Tyre', image: polo16, vote: null },
-      
+      { id: 'polo-11', name: 'Allen Solly | Black Half Sleeve', image: polo11, vote: null },
+      { id: 'polo-12', name: 'Superconcepts | Navy Blue Full Sleeve Ribbed', image: polo12, vote: null },
+      { id: 'polo-13', name: 'Tyre | Black & Beige Full Sleeve Tyre', image: polo13, vote: null },
+      { id: 'polo-14', name: 'NUS | Navy Blue Half Sleeve Jersey', image: polo14, vote: null },
+      { id: 'polo-15', name: 'India | Blue Half Sleeve Jersey', image: polo15, vote: null },
+      { id: 'polo-16', name: 'CSK | Yellow Half Sleeve Jersey', image: polo16, vote: null },
+      { id: 'polo-17', name: 'CSK | Yellow Half Sleeve Jersey', image: polo17, vote: null },
     ]
   },
   {
@@ -138,22 +207,72 @@ export const clothingCategories: ClothingCategory[] = [
       { id: 'shirt-16', name: 'Cherokee | Olive Green', image: shirt16, vote: null },
       { id: 'shirt-17', name: 'Bare Denim | Brown', image: shirt17, vote: null },
       { id: 'shirt-18', name: 'Bare Denim | Blue Checked', image: shirt18, vote: null },
+      { id: 'shirt-19', name: 'Banana Club | Gray', image: shirt19, vote: null },
     ]    
   },
   {
     id: 'tshirts',
     name: 'T SHIRTS',
     items: [
-      { id: 'tshirt-1', name: 'Black T-Shirt', image: tshirt1, vote: null },
-      { id: 'tshirt-2', name: 'White T-Shirt', image: tshirt2, vote: null },
+      { id: 'tee-1', name: '', image: tee1, vote: null },
+      { id: 'tee-2', name: '', image: tee2, vote: null },
+      { id: 'tee-3', name: '', image: tee3, vote: null },
+      { id: 'tee-4', name: '', image: tee4, vote: null },
+      { id: 'tee-5', name: '', image: tee5, vote: null },
+      { id: 'tee-6', name: '', image: tee6, vote: null },
+      { id: 'tee-7', name: '', image: tee7, vote: null },
+      { id: 'tee-8', name: '', image: tee8, vote: null },
+      { id: 'tee-9', name: '', image: tee9, vote: null },
+      { id: 'tee-10', name: '', image: tee10, vote: null },
+      { id: 'tee-11', name: '', image: tee11, vote: null },
+      { id: 'tee-12', name: '', image: tee12, vote: null },
+      { id: 'tee-13', name: '', image: tee13, vote: null },
+      { id: 'tee-14', name: '', image: tee14, vote: null },
+      { id: 'tee-15', name: '', image: tee15, vote: null },
+      { id: 'tee-16', name: '', image: tee16, vote: null },
+      { id: 'tee-17', name: '', image: tee17, vote: null },
+      { id: 'tee-18', name: '', image: tee18, vote: null },
+      { id: 'tee-19', name: '', image: tee19, vote: null },
+      { id: 'tee-20', name: '', image: tee20, vote: null },
+    ]
+  },
+  {
+    id: 'graphictee',
+    name: 'GRAPHIC TEES',
+    items: [
+      { id: 'gtee-1', name: '', image: gtee1, vote: null },
+      { id: 'gtee-2', name: '', image: gtee2, vote: null },
+      { id: 'gtee-3', name: '', image: gtee3, vote: null },
+      { id: 'gtee-4', name: '', image: gtee4, vote: null },
+      { id: 'gtee-5', name: '', image: gtee5, vote: null },
+      { id: 'gtee-6', name: '', image: gtee6, vote: null },
+      { id: 'gtee-7', name: '', image: gtee7, vote: null },
+      { id: 'gtee-8', name: '', image: gtee8, vote: null },
+      { id: 'gtee-9', name: '', image: gtee9, vote: null },
+      { id: 'gtee-10', name: '', image: gtee10, vote: null },
+      { id: 'gtee-11', name: '', image: gtee11, vote: null },
+      { id: 'gtee-12', name: '', image: gtee12, vote: null },
+      { id: 'gtee-13', name: '', image: gtee13, vote: null },
+      { id: 'gtee-14', name: '', image: gtee14, vote: null },
+      { id: 'gtee-15', name: '', image: gtee15, vote: null },
+      { id: 'gtee-16', name: '', image: gtee16, vote: null },
+      { id: 'gtee-17', name: '', image: gtee17, vote: null },
+      { id: 'gtee-18', name: '', image: gtee18, vote: null },
+      { id: 'gtee-19', name: '', image: gtee19, vote: null },
+      { id: 'gtee-20', name: '', image: gtee20, vote: null },
+      { id: 'gtee-21', name: '', image: gtee21, vote: null },
+      { id: 'gtee-22', name: '', image: gtee22, vote: null },
     ]
   },
   {
     id: 'hoodies',
     name: 'HOODIES',
     items: [
-      { id: 'hoodie-1', name: 'Grey Hoodie', image: hoodie1, vote: null },
-      { id: 'hoodie-2', name: 'Black Hoodie', image: hoodie2, vote: null },
+      { id: 'hoodie-1', name: 'GAP | Gray', image: hoodie1, vote: null },
+      { id: 'hoodie-2', name: 'H&M | Gray', image: hoodie2, vote: null },
+      { id: 'hoodie-3', name: 'Nike | Red', image: hoodie3, vote: null },
+      { id: 'hoodie-4', name: 'Westside | All', image: hoodie4, vote: null },
+      { id: 'hoodie-5', name: 'Pepe Jeans | White Gray Yellow', image: hoodie5, vote: null },
     ]
   },
   {
@@ -164,8 +283,26 @@ export const clothingCategories: ClothingCategory[] = [
       { id: 'sweatshirt-2', name: 'Marks & Spencer | Black & Gray', image: sweatshirt2, vote: null },
       { id: 'sweatshirt-3', name: 'Marks & Spencer | Light Blue', image: sweatshirt3, vote: null },
       { id: 'sweatshirt-4', name: 'H&M | Violet', image: sweatshirt4, vote: null },
-      { id: 'sweatshirt-5', name: 'The Souled Store | Organge Red', image: sweatshirt5, vote: null },
+      { id: 'sweatshirt-5', name: 'The Souled Store | Orange Red', image: sweatshirt5, vote: null },
       { id: 'sweatshirt-6', name: 'Wrogn | Black', image: sweatshirt6, vote: null },
+      { id: 'sweatshirt-7', name: 'H&M | White', image: sweatshirt7, vote: null },
+      { id: 'sweatshirt-8', name: 'Marks & Spencer | Brown', image: sweatshirt8, vote: null },
+      { id: 'sweatshirt-9', name: 'Zara | Maroon', image: sweatshirt9, vote: null },
+    ]
+  },
+  {
+    id: 'tracks',
+    name: 'TRACKS',
+    items: [
+      { id: 'tracks-1', name: '', image: tracks1, vote: null },
+      { id: 'tracks-2', name: '', image: tracks2, vote: null },
+      { id: 'tracks-3', name: '', image: tracks3, vote: null },
+      { id: 'tracks-4', name: '', image: tracks4, vote: null },
+      { id: 'tracks-5', name: '', image: tracks5, vote: null },
+      { id: 'tracks-6', name: '', image: tracks6, vote: null },
+      { id: 'tracks-7', name: '', image: tracks7, vote: null },
+      { id: 'tracks-8', name: '', image: tracks8, vote: null },
+      { id: 'tracks-9', name: '', image: tracks9, vote: null },
     ]
   },
 ];
